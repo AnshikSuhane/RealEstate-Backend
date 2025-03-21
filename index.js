d8a299b16381dc7750f503107f7b8f29534f0711
AnshikSuhane <anshiksuhane9@gmail.com>
2025-03-21 20:11:15 +0530

    refactor server setup and add welcome route

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { userRoute } from './routes/userRoute.js';
 import { residencyRoute } from './routes/residencyRoute.js';
-dotenv.config()
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
+// Welcome Route
+app.get('/api', (req, res) => {
+    res.send('Welcome to RealEState Server');
 });
 
-app.use('/api/user', userRoute)
-app.use("/api/residency", residencyRoute)
+app.use('/api/user', userRoute);
+app.use('/api/residency', residencyRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
